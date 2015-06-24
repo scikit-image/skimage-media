8233a68702f985c9f6e59011dafc3a8fd8531c3c
Josh Warner (Mac) <warner.joshua@mayo.edu>
2015-06-24 01:06:09 -0500

    Simplify astronaut script with imsave

--- handout/data/astronaut.py
+++ handout/data/astronaut.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 from skimage.data import astronaut
+import skimage.io
 
 
 if __name__ == '__main__':
     # Save local copy of the Astronaut test image for use in LaTeX
-    dpi = 100.0
-    xpixels, ypixels = 512, 512
-
-    # This yields a bit-perfect (512, 512) sized PNG with no margins
-    fig = plt.figure(figsize=(ypixels / dpi, xpixels / dpi), dpi=dpi)
-    fig.figimage(astronaut())
-    fig.savefig('./astronaut.png')
+    skimage.io.imsave('./astronaut.png', astronaut())
